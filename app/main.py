ee1321945f50f551af3916b6e18e0018e54a8a28
Дмитрий Хома <dmitriykhoma@MacBook-Pro-Dmitrij.local>
2026-05-27 09:35:02 -0400

    oauth auto redirect fix

--- app/main.py
+++ app/main.py
@@ -1813,45 +1813,12 @@ def oauth_callback(code: str = ""):
             "error": "Missing OAuth code"
         }
 
-    return HTMLResponse(f"""
-    <html>
-    <body style="
-        background:#081225;
-        color:white;
-        font-family:Arial;
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        height:100vh;
-    ">
+    
+    return RedirectResponse(
+        url="/broker",
+        status_code=302
+    )
 
-    <div style="
-        background:#111827;
-        padding:40px;
-        border-radius:20px;
-        width:500px;
-        text-align:center;
-    ">
-
-    <h1>Tradovate Connected</h1>
-
-    <p>OAuth connection successful.</p>
-
-    <div style="
-        background:black;
-        padding:15px;
-        border-radius:10px;
-        word-break:break-all;
-        margin-top:20px;
-    ">
-    {code}
-    </div>
-
-    </div>
-
-    </body>
-    </html>
-    """)
 
 
 
